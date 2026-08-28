34e7a668a3a0a666c3861b1e9eec22c9065c84bc
Alluri Eswar Varma <eswar.ev856@gmail.com>
2026-08-28 09:42:52 +0530

    Time: 6 ms (93.37%) | Memory: 20 MB (71.94%) - LeetSync

---
+++ 1775-design-an-ordered-stream/design-an-ordered-stream.py
@@ -0,0 +1,16 @@
+class OrderedStream:
+
+    def __init__(self, n: int):
+        self.stream = [None] * (n + 1)
+        self.ptr = 1
+
+    def insert(self, idKey: int, value: str) -> List[str]:
+        self.stream[idKey] = value
+        res = []
+        
+        # Collect continuous available values starting from self.ptr
+        while self.ptr < len(self.stream) and self.stream[self.ptr] is not None:
+            res.append(self.stream[self.ptr])
+            self.ptr += 1
+            
+        return res
