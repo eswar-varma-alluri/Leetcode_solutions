e8af528f1118be4cba68a009f9e17d354b5fbaaa
Alluri Eswar Varma <eswar.ev856@gmail.com>
2026-08-29 11:29:58 +0530

    Time: 216 ms (61.36%) | Memory: 15 MB (91.93%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def ispossible(self,nums,days,mid):
+        load=0
+        rdays=1
+        for w in nums:
+            if load+w <=mid:
+                load+=w
+            else:
+                load=w
+                rdays+=1
+        return rdays<=days
+    def shipWithinDays(self, nums, days):
+        l=max(nums)
+        h=sum(nums)
+        while l<h:
+            mid=(l+h)//2
+            nd= self.ispossible(nums,days,mid)
+            if nd:
+                h=mid
+            else:
+                l=mid+1
+        return l
